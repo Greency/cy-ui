228fca265421124eeb901da05935c3f7077cc914
chenyong <1015561170@qq.com>
2019-06-04 14:55:03 +0800

    feat：popup组件

---
+++ ui/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+	<div>
+		<button @click="setting">setting</button>
+		<cy-popup v-model="show">
+			<div style="width: 375px;height: 300px;background-color: red;"></div>
+		</cy-popup>
+	</div>
+</template>
+
+<script>
+import Popup from './packages/popup';
+
+export default {
+	name: 'app',
+	components: {
+		'cy-popup': Popup
+	},
+	data() {
+		return {
+			show: false
+		}
+	},
+	methods: {
+		setting: function(){
+			this.show = !this.show;
+		}
+	}
+}
+</script>
+
+<style>
+</style>
