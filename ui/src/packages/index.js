4d3dea0cd44ea810ff01fc64c49ef1f91a09ca85
chenyong <1015561170@qq.com>
2019-06-25 15:31:10 +0800

    feat: 将组件封装成插件

--- ui/src/packages/index.js
+++ ui/src/packages/index.js
@@ -1,7 +1,36 @@
-import Button from './button'
+import './bindPrototype';
+import Button from './button';
+import Popup from './popup';
+import Switch from './switch';
+import Modal from './modal';
+import SwipeCellGroup from './swipeCell/swipeCellGroup';
+import SwipeCell from './swipeCell/swipeCell';
+import Picker from './picker/picker';
+import PickerColumn from './picker/pickerColumn';
+import RadioGroup from './radio/radioGroup';
+import Radio from './radio/radio';
+import CheckboxGroup from './checkbox/checkboxGroup';
+import Checkbox from './checkbox/checkbox';
+
+const COMPONENTS_LIST = [
+    { name: 'button', component: Button },
+    { name: 'popup', component: Popup },
+    { name: 'switch', component: Switch },
+    { name: 'modal', component: Modal },
+    { name: 'swipe-cell-group', component: SwipeCellGroup },
+    { name: 'swipe-cell', component: SwipeCell },
+    { name: 'picker', component: Picker },
+    { name: 'picker-column', component: PickerColumn },
+    { name: 'radio-group', component: RadioGroup },
+    { name: 'radio', component: Radio },
+    { name: 'checkbox-group', component: CheckboxGroup },
+    { name: 'checkbox', component: Checkbox }
+];
 
 function install(Vue) {
-    Vue.component('cy-button', Button);
+    COMPONENTS_LIST.forEach((item)=>{
+        Vue.component(`cy-${item.name}`, item.component);
+    });
 }
 
 let CyUI = {
